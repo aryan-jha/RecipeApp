b5687fa8cc47ca5feb7f290906f5eac6e69a8077
Aryan Jha <jhaaryan2004@gmail.com>
2024-04-14 03:06:23 +0530

    added the permission and finalized the app

--- app/src/main/java/com/example/myrecipeapp/ApiService.kt
+++ app/src/main/java/com/example/myrecipeapp/ApiService.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 private val retrofit by lazy {
-        Retrofit.Builder().baseUrl("www.themealdb.com/api/json/v1/1/")
+        Retrofit.Builder().baseUrl("https://www.themealdb.com/api/json/v1/1/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 }
